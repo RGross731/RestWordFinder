f1d3a8788483b21664ab81a86cbc8954f0554135
rgros <rgros@DESKTOP-AHJCEQG>
2019-02-26 21:56:28 -0600

    Additional dependencies and package structure

--- src/main/java/org/gross/ryan/restwordfinder/RestWordFinderApplication.java
+++ src/main/java/org/gross/ryan/restwordfinder/RestWordFinderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestWordFinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestWordFinderApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RestWordFinderApplication.class, args);
+  }
 
 }
